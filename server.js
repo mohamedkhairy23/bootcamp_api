322fb66c04eb1d4626c7fdc25fa88a22a45beb80
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-06 16:40:13 +0200

    Database Connection

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const bootcamps = require("./routes/bootcamps");
+dotenv.config({ path: "./config/.env" });
+const connectDB = require("./config/db");
 // const logger = require("./middleware/logger");
+const colors = require("colors");
 const morgan = require("morgan");
 
-dotenv.config({ path: "./config/.env" });
+const bootcamps = require("./routes/bootcamps");
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // app.use(logger);
 if ((process.env.NODE_ENV = "development")) {
@@ -17,8 +24,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+// handle unhandle process rejections
+process.on("unhandleRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server and exit process
+  // server.close(() => process.exit(1));
+});
