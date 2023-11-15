b3b0232a3230617af40a0052f23a8b851ffbd7c2
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-15 19:05:49 +0200

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,7 +21,42 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // generate token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+    data: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
+});
+
+// @desc     login a user
+// @route    GET /api/v1/auth/register
+// @access   Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse(`Please provide an email and password`, 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse(`Invalid email or password`, 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse(`Invalid email or password`, 401));
+  }
+
   const token = user.getSignedJwtToken();
 
   res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -44,11 +44,14 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// to generate token
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
+};
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model("User", UserSchema);
