106278b18f7a5651dc2c93a975b004d826e43b6a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-17 15:15:37 +0200

    Get reset password token

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,11 +79,32 @@ const sendTokenResponse = (user, statusCode, res) => {
 };
 
 // @desc     Get current logged in user
-// @route    Post /api/v1/auth/me
+// @route    GET /api/v1/auth/me
 // @access   Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   // const user = await User.findById(req.user.id);
   const user = req.user;
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc     Forgot Password
+// @route    POST /api/v1/auth/forgotpassword
+// @access   Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse("There is no user with that email", 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
 
   res.status(200).json({
     success: true,
