787d8de4bd17a9e4e07291b7466cc114816d285a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-18 13:34:06 +0200

    Encrypt password using bcrypt while updating user by admin

--- models/User.js
+++ models/User.js
@@ -45,6 +45,14 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Encrypt password using bcrypt while updating user by admin
+UserSchema.pre("findOneAndUpdate", async function (next) {
+  if (this._update.password) {
+    this._update.password = await bcrypt.hash(this._update.password, 10);
+  }
+  next();
+});
+
 // generate token
 UserSchema.methods.getSignedJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
