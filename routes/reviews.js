8d802b62b76f12133187cf0f8e80f55280dde9c1
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-18 19:31:55 +0200

    get single review and populate some bootcamp and user info

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getReviews } = require("../controllers/reviews");
+const { getReviews, getReview } = require("../controllers/reviews");
 const Review = require("../models/Review");
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
@@ -8,16 +8,14 @@ const router = express.Router({ mergeParams: true });
 
 router.route("/").get(
   advancedResults(Review, {
-    path: "bootcamp",
-    select: "name description",
+    path: "bootcamp user",
+    select: "name description email",
   }),
   getReviews
 );
 
-// router
-//   .route("/:id")
-//   .get(getCourse)
-//   .put(protect, authorize("publisher", "admin"), updateCourse)
-//   .delete(protect, authorize("publisher", "admin"), deleteCourse);
+router.route("/:id").get(getReview);
+// .put(protect, authorize("publisher", "admin"), updateCourse)
+// .delete(protect, authorize("publisher", "admin"), deleteCourse);
 
 module.exports = router;
