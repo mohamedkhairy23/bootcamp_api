674a9d0d4a08ec329c00b17d0c50e321f34b128f
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-11-18 21:08:05 +0200

    user can add only one review on a specific bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -9,7 +9,12 @@ const Bootcamp = require("../models/Bootcamp");
 // @access   Public
 exports.getReviews = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
-    const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+    const reviews = await Review.find({
+      bootcamp: req.params.bootcampId,
+    }).populate({
+      path: "user",
+      select: "name email",
+    });
 
     return res.status(200).json({
       success: true,
@@ -48,23 +53,37 @@ exports.getReview = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc     Update a review
-// @route    GET /api/v1/reviews/:id
-// @access   Public
-exports.updateReview = asyncHandler(async (req, res, next) => {
-  const review = await Review.findById(req.params.id).populate({
-    path: "bootcamp",
-    select: "name description",
-  });
+// @desc     Add  review
+// @route    POST /api/v1/reviews/:bootcampId/reviews
+// @access   Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  // Add bootcamp to req.body by bootcamp ID
+  req.body.bootcamp = req.params.bootcampId;
 
-  if (!review) {
+  // Add logged in user to req.body
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  if (!bootcamp) {
     return next(
-      new ErrorResponse(`No review found with the id of ${req.params.id}`),
-      404
+      new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`, 404)
     );
   }
 
-  res.status(200).json({
+  const alreadyReviewed = await Review.findOne({
+    bootcamp: req.params.bootcampId,
+    user: req.user.id,
+  });
+
+  if (alreadyReviewed) {
+    return next(
+      new ErrorResponse(`You already added review on this bootcamp`, 401)
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(201).json({
     success: true,
     data: review,
   });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const { getReviews, getReview } = require("../controllers/reviews");
+const { getReviews, getReview, addReview } = require("../controllers/reviews");
 const Review = require("../models/Review");
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(
-  advancedResults(Review, {
-    path: "bootcamp user",
-    select: "name description email",
-  }),
-  getReviews
-);
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp user",
+      select: "name description email",
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 
 router.route("/:id").get(getReview);
 // .put(protect, authorize("publisher", "admin"), updateCourse)
